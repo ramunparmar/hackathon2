f20b0deef098c94c9493dd714e736e4e76da4045
Anthony Henley <anthonyhenley@Anthonys-MacBook-Air.local>
2021-06-11 15:45:34 -0400

    Initialized AucitonPage styles

--- client/src/pages/ArtistPage/ArtistPage.js
+++ client/src/pages/ArtistPage/ArtistPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from "react";
 import axios from "axios";
+import './ArtistPage.scss';
 
 class ArtistPage extends Component {
 
@@ -32,7 +33,16 @@ class ArtistPage extends Component {
 
         return (
             <section className="artist">
-                <h1 className="artist__header">Artist Page</h1>
+              <div className="artist__hero">
+                <p className="artist__genre">Indie-Rock</p>
+              </div>
+              <div>
+                <div className="artist__header">
+                  <h1 className="artist__header--title">The Scotts</h1>
+                  <h2 className="artist__header--followers">33,420 followers</h2>
+                  <h2 className="artist__header--social">@official_thescotts</h2>
+                </div>
+              </div>
                 {console.log(this.state.artists.data)}
             </section>
     
--- client/src/pages/AuctionPage/AuctionPage.js
+++ client/src/pages/AuctionPage/AuctionPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Component } from "react";
+import BackArrow from '../../assets/icons/arrow_back_purple.svg';
+import Bookmark from '../../assets/icons/bookmark.svg';
 import axios from "axios";
+import './AuctionPage.scss';
 
 class AuctionPage extends Component {
 
@@ -32,7 +35,35 @@ class AuctionPage extends Component {
 
         return (
             <section className="auction">
-                <h1 className="auction__header">Auction Page</h1>
+              <nav className="auction__nav">
+                  <img className="auction__icon" src={BackArrow} alt="menu"/>
+                  <h1>Details</h1>
+                  <img className="auction__icon" src={Bookmark} alt="user"/>
+              </nav>
+              <div className="auction__item">
+                <div className="auction__item__container">
+                  <div className="auction__item__preview">
+                  ▶ Preview
+                  </div>
+                  <div className="auction__item__details">
+                    <h1>Outside</h1>
+                    <h2>The Scotts</h2>
+                    <p>This song was created as a digital asset to be sold on the NFT marketplace. Only twenty NFTs will ever be released associated with this song.</p>
+                  </div>
+                  <div className="auction__item__bid">
+                    
+                  </div>
+                  <div className="auction__item__button">
+                    <button>Make an Offer</button>
+                  </div>
+                </div>
+              </div>
+              <div className="auction__offers">
+                <h1 className="auction__offers__header">Offer History</h1>
+                <div>Offer 1</div>
+                <div>Offer 2</div>
+                <div>Offer 3</div>
+              </div>
                 {console.log(this.state.products.data)}
             </section>
     
