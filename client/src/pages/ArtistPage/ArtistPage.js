99ec086f5ef4c63b7aac98a3716e118f40ccb862
Anthony Henley <anthonyhenley@Anthonys-MacBook-Air.local>
2021-06-11 09:34:17 -0400

    Connected frontend to server via axios for artist and product routes

--- client/src/pages/ArtistPage/ArtistPage.js
+++ client/src/pages/ArtistPage/ArtistPage.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Component } from "react";
+import axios from "axios";
 
-function ArtistPage() {
-    return (
-        <section className="artist">
-            <h1 className="artist__header">Artist Page</h1>
-        </section>
+class ArtistPage extends Component {
 
-    )
+    state = {
+        artists: [],
+    }
+
+    getArtist() {
+        axios
+          .get(`http://localhost:5000/artists`)
+          .then((artist) => {
+            this.setState({
+              artists: artist,
+            });
+          })
+          .catch((err) => console.error(err));
+      }
+    
+    componentDidMount() {
+        this.getArtist();
+    }
+    
+
+    render() {
+
+        if (this.state.artist === []) {
+            return <h1>Loading. . .</h1>;
+          }
+
+        return (
+            <section className="artist">
+                <h1 className="artist__header">Artist Page</h1>
+                {console.log(this.state.artists.data)}
+            </section>
+    
+        )
+    }
 }
 
 export default ArtistPage;
